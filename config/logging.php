fe89671b7801235af706a82cc4de0f3f9c2ac8f3
jeiberth <jeiberth_camilo@hotmail.com>
2025-04-28 19:03:54 -0400

    commands for deployment added

--- config/logging.php
+++ config/logging.php
@@ -18,7 +18,9 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    // 'default' => env('LOG_CHANNEL', 'stack'),
+    'default' => env('LOG_CHANNEL', 'stderr'),
+
 
     /*
     |--------------------------------------------------------------------------
